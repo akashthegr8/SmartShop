826d358b4dfe307762ffd23b70dd1f484ad1a90a
Abhivyakt Shivarudrappa Rajeshwari <abhivyakt.shivarudrappa.rajeshwari@sap.com>
2016-12-13 01:43:26 +0530

    abhiv2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
 //top view controller
 .controller('AppCtrl', function($scope, $rootScope, UserService, $ionicActionSheet, $state, $ionicLoading) {
@@ -275,5 +275,81 @@ angular.module('starter.controllers', [])
     $scope.cart = CartService.resetCart();
     $state.go('app.shop')
   }
+
+})
+
+.controller('WishlistCtrl', function($scope, $ionicPopup) {
+  
+  $scope.addItem = function(){
+    
+    // Add Item popup
+  var myPopup = $ionicPopup.show({
+    template: '<input type="text" placeholder="Item Category"><br><input type="text" placeholder="Item name">',
+    title: '<b>Add Item to Wishlist</b>',
+    cssClass: 'my-custom-popup',
+    buttons: [
+      { text: '<b>Cancel</b>',
+        type: 'button-positive',
+        onTap: function(){
+          alert("Cancel");
+        } },
+      {
+        text: '<b>Save</b>',
+        type: 'button-positive',
+        onTap: function() {
+          alert("Save");
+        }
+      }
+    ]
+  });
+
+  };
+
+  $scope.navigate = function(){
+
+    // Choose mall
+  var myPopup2 = $ionicPopup.show({
+    template: '<input type="text" placeholder="Mall/Shop name">>',
+    title: '<b>Add Mall/Shop</b>',
+    cssClass: 'my-custom-popup',
+    buttons: [
+      { text: '<b>Cancel</b>',
+        type: 'button-positive',
+        onTap: function(){
+          alert("Cancel");
+        } },{
+        text: '<b>Proceed</b>',
+        type: 'button-positive',
+        onTap: function() {
+          alert("Navigate to map screen");
+        }
+      }
+    ]
+  });
+
+  };
+
+})
+
+
+.controller('RouteCtrl', function($scope) {
+  
+  
+
+})
+
+.controller('ShopDetailsCtrl', function($scope) {
+  
+  $scope.rateShop = function(){
+
+  };
+
+  $scope.markFav = function(){
+
+  };
+
+  $scope.addReview = function(){
+
+  };
 
 })
